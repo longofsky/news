7df3234b734891cd51bdcf6c97937ac3bc461d90
huahaoran <huahaoran@patsnap.com>
2018-05-25 17:10:07 +0800

    增加微信消息的处理类

---
+++ src/main/java/com/patent/news/util/OutputMessage.java
@@ -0,0 +1,54 @@
+package com.patent.news.util;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+@XStreamAlias("xml")
+public class OutputMessage {
+
+    @XStreamAlias("ToUserName")
+    @XStreamCDATA
+    private String ToUserName;
+
+    @XStreamAlias("FromUserName")
+    @XStreamCDATA
+    private String FromUserName;
+
+    @XStreamAlias("CreateTime")
+    private Long CreateTime;
+
+    @XStreamAlias("MsgType")
+    @XStreamCDATA
+    private String MsgType = "text";
+
+    public String getToUserName() {
+        return ToUserName;
+    }
+
+    public void setToUserName(String toUserName) {
+        ToUserName = toUserName;
+    }
+
+    public String getFromUserName() {
+        return FromUserName;
+    }
+
+    public void setFromUserName(String fromUserName) {
+        FromUserName = fromUserName;
+    }
+
+    public Long getCreateTime() {
+        return CreateTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        CreateTime = createTime;
+    }
+
+    public String getMsgType() {
+        return MsgType;
+    }
+
+    public void setMsgType(String msgType) {
+        MsgType = msgType;
+    }
+}
